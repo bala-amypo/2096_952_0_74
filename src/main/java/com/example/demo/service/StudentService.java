e137eacf12b7beacbc57a2a8c92c0fdfcf167cff
bala-amypo <bala@amypo.in>
2025-12-17 08:09:51 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,6 +1,12 @@
 package com.example.demo.service;
+import org.springframework.beans.factory.anotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+import com.example.demo.repository.StudenRespository;
 public interface StudentService{
+    StudentRepository stdrepo;
     Student poststudent(Student st);
     return 
 }
