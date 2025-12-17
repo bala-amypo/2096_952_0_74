24974daecea2ae82947ed9dc9f94bad792d264a9
bala-amypo <bala@amypo.in>
2025-12-17 08:14:47 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -6,7 +6,10 @@ import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
 import com.example.demo.repository.StudenRespository;
 public interface StudentService{
+    @Autowired
     StudentRepository stdrepo;
+    @Override
+    
     Student poststudent(Student st);
     return 
 }
