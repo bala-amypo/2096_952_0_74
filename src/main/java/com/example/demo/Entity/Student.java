c88a065e6594d98aa43f9932330a80f6f32c834c
bala-amypo <bala@amypo.in>
2025-12-17 05:11:10 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Student.java
+++ src/main/java/com/example/demo/Entity/Student.java
@@ -2,37 +2,58 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 import jakarta.persistence.*;
-import jakarta.persistence.Entity;
+
 @Entity
-public class Student{
+public class Student {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String dept;
-    Private LocalDate dob;
+    private LocalDate dob;
     private float cgpa;
-    public Long getId(){
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String dept, LocalDate dob, float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.dob = dob;
+        this.cgpa = cgpa;
+    }
+
+    public Long getId() {
         return id;
     }
-    public void setId(Long id){
-        this.id=id;
+
+    public void setId(Long id) {
+        this.id = id;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void setName(String name){
-        this.name=name;
+
+    public void setName(String name) {
+        this.name = name;
     }
+
     public String getDept() {
         return dept;
     }
+
     public void setDept(String dept) {
         this.dept = dept;
-    }    
+    }
+
     public LocalDate getDob() {
         return dob;
     }
+
     public void setDob(LocalDate dob) {
         this.dob = dob;
     }
@@ -40,17 +61,8 @@ public class Student{
     public float getCgpa() {
         return cgpa;
     }
-    public void setCgpa(float cgpa){
-        this.cgpa=cgpa;
-    }
-    public Student(){
 
-    }
-    public Student(Long id,String name,String dept,LocalDate dob,float cgpa){
-       this.id=id;
-       this.department;
-       this.dept=dept;
-       this.dob=dob;
-       this.cgpa=cgpa;
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
     }
 }
