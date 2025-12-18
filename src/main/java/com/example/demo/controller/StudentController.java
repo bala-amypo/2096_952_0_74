1b5dde5667b32396ea9c4a51712c6509f0f5931a
bala-amypo <bala@amypo.in>
2025-12-18 05:33:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -27,5 +27,8 @@ public class StudentController {
     public Optional getId(@PathVariable Long id) {
         return stdser.getById(id);
     }
-    
+    @PutMapping("/update/{id}")
+    public String update(@PathVariable Long id,@RequestBody Student st){
+        return stdser.updateData(id,st);
+          }
 }
